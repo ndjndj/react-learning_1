f34528d7715506f4b5c40d3be23ba00e3d8d0348
ndjndj <teq.ndj@gmail.com>
2020-10-01 03:23:44 +0900

    text area

---
+++ stopwatch/src/InputArea.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const InputArea = (props) => {
+  return(
+    <Area></Area>
+  );
+}
+
+export default InputArea;
+
+const Area = styled.textarea`
+  width: 60%;
+  height: 300px;
+  padding: 5px;
+  margin: 2%;
+  font-family: 'Montserrat', sans-serif;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  font-size: 24px;
+  appearance: none;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+`
