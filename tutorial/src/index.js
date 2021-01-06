11525acbef6b58c4cabdba66d2c147f1974ed360
ndjndj <teq.ndj@gmail.com>
2021-01-06 15:05:26 +0900

    add validate

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -24,6 +24,11 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
